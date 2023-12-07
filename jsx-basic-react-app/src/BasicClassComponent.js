0b52adb024e8deff4a68b98887c4dadc225fc7c3
dttrue <danieltorres@pursuit.org>
2023-12-07 11:38:44 -0500

    Daniels add BasicClassComponent

---
+++ jsx-basic-react-app/src/BasicClassComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BasicClassComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello! This is a Basic Class Component, which is a class extending React.Component, and is rendered from jsx-basic-react-app/src/App.js  </h1>
+            </div>
+        );
+    }
+}
+
+export default BasicClassComponent
