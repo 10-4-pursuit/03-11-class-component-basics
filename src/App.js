d70b9f82aed3141f4ea4e8a795eea8062ba683a2
athomas227 <andrewthomas@pursuit.org>
2023-12-09 17:55:53 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BasicClassComponent from './BasicClassComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <BasicClassComponent />
+    </div>
+  );
+}
+
+export default App;
