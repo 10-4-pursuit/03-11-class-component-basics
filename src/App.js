e74869fb1f1514ed860288ef334bfeca44ce89fa
clairep9 <clairepetitfrere@pursuit.org>
2023-12-23 11:19:57 -0500

    Add BasicClassComponent

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicClassComponent from './BasicClassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicClassComponent />
+    </div>
+  );
+}
+
+export default App;
