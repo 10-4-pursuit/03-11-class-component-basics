d4cb96308eedef5441c467e879c842600d2f7c14
SaraM09 <sayeramusfika@pursuit.org>
2023-12-28 01:07:46 -0500

    Add BasicClassComponent

---
+++ 03-11-exercise/src/BasicClassComponent.js
@@ -0,0 +1,8 @@
+ import React from 'react';
+ class BasicClassComponent extends React.Component {
+render() {
+     return <div>This is a basic class component</div>;
+}
+   
+ }
+ export default BasicClassComponent
