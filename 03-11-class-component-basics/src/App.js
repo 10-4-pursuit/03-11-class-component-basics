3488d7e7e640b62e16e71285ae23efd93a27f2dd
lanell1 <lanelllopez@pursuit.org>
2024-01-16 18:50:31 -0500

    complete-03-11-class-component-basics

---
+++ 03-11-class-component-basics/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import BasicClassComponent from './BasicClassComponent';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BasicClassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 03-11-class-component-basics/src/BasicClassComponent.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class BasicClassComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello from Class Component!
+      </div>
+    );
+  }
+}
+
+export default BasicClassComponent;
