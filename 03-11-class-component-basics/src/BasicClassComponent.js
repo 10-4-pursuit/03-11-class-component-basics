b4136b6efc1c40073f23ed169aa177ea30ab331a
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2024-01-28 09:01:25 -0500

    class component is workinggit add .

---
+++ 03-11-class-component-basics/src/BasicClassComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BasicClassComponent extends Component {
+        render() {
+            return (
+                <div>
+                    <p> Hello from Class Component !!!</p>
+                </div>
+            );
+        }
+    }
+    export default BasicClassComponent;
